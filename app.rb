f37b7d9008691dfdf5acc50afc1dec63d3fcd913
Emmanuel <rivera128@live.com>
2021-01-27 00:25:29 -0600

    commit tree 780ae5bdcad92812549ac7ff12194b7b3df21998

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+    end
+
+    post "/piglatinize" do
+        @pig_latinized_word = PigLatinizer.new
+        @pig_latinized_word.piglatinize(params[:user_phrase])
+    end
 end
